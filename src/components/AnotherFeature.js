de631a3ea6ecacf1662f7448e901ea67af1f5676
xqcao <xqcaoumail@gmail.com>
2022-03-13 03:47:18 -0400

    main another feature version 1

---
+++ src/components/AnotherFeature.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AnotherFeature = () => {
+  return (
+    <div>
+      <h2>Another Feature</h2>
+      <p>this is version 1</p>
+    </div>
+  );
+};
+
+export default AnotherFeature;

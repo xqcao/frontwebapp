310837303ae65cc81cc100c1500c7008143f0170
xqcao <xqcaoumail@gmail.com>
2022-03-13 01:09:06 -0500

    main_b add  section_1 to section_3

--- src/components/NewBacklog.js
+++ src/components/NewBacklog.js
@@ -6,6 +6,18 @@ const NewBacklog = () => {
       <h2>New Feature request from boss</h2>
       <p>please add more css ane js make webpage mobile friendly</p>
       <h2>Ok, one dev start working on this and add this message</h2>
+      <sction>
+        <i>1</i>
+        <h2>Please add view button here and make page nice</h2>
+      </sction>
+      <sction>
+        <i>2</i>
+        <h2>Please add background image here, and page looks not that empty</h2>
+      </sction>
+      <sction>
+        <i>3</i>
+        <h2>add customer feedback form, so can improve performance</h2>
+      </sction>
     </div>
   );
 };

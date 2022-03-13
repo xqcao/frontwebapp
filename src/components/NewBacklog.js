6a2fab1cc3c2920387a565d6fc8be74cd4822915
xqcao <xqcaoumail@gmail.com>
2022-03-12 21:50:26 -0500

    add new feature to backlog, and create newbacklog.js page

---
+++ src/components/NewBacklog.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewBacklog = () => {
+  return (
+    <div>
+      <h2>New Feature request from boss</h2>
+      <p>please add more css ane js make webpage mobile friendly</p>
+    </div>
+  );
+};
+
+export default NewBacklog;

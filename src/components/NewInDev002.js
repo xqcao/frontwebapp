a2a4b24f6c820b715ff255e3701c9c0af2b8ebee
xqcao <xqcaoumail@gmail.com>
2022-03-13 00:46:49 -0500

    dev002 ----version 1 edit

---
+++ src/components/NewInDev002.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewInDev002 = () => {
+  return (
+    <div>
+      <h2>NewInDev002</h2>
+      <p>111111</p>
+    </div>
+  );
+};
+
+export default NewInDev002;
